0993d40cd298308ecbe18f354b8073269a7cc303
Hannah Joynson <hannahjoynson@gmail.com>
2022-05-16 14:35:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import './App.css';
+
+
+const App = () => {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await
+      console.log("function called")
+
+    }
+  })
+
+}
+export default App
